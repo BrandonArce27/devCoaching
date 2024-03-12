8f4fb5c78992724b37abf96332d791d5e9873c6a
BrandonArce27 <brandonarce2712@gmail.com>
2024-03-11 23:35:48 -0600

    chore(files): create files and decorators

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { UserModule } from '@api';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+      driver: ApolloDriver,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
