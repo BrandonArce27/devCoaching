702736bd2e7d8d6554cdbe2f73fa2c5ebaf93c37
BrandonArce27 <brandonarce2712@gmail.com>
2024-04-18 14:39:46 -0600

    chore(app): adding entities imports to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,19 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { UserModule } from '@api';
 
+import { ReviewModule } from '@api';
+
+import { TicketModule } from '@api';
+
 import { KnowledgeModule } from '@api';
 
 import { AcademyModule } from '@api';
+
+import { AuthModule } from '@auth';
+
+import { ConfigModule } from '@config';
+
+import { PrismaModule } from './shared/services';
 
 @Module({
   imports: [
@@ -17,9 +27,14 @@ import { AcademyModule } from '@api';
       playground: true,
       driver: ApolloDriver,
     }),
+    AuthModule,
     UserModule,
+    ConfigModule,
     AcademyModule,
     KnowledgeModule,
+    PrismaModule,
+    ReviewModule,
+    TicketModule,
   ],
   controllers: [],
   providers: [],
