d339178df76584b7b07229fdee22642bc66b31e9
BrandonArce27 <brandonarce2712@gmail.com>
2024-04-18 14:38:28 -0600

    chore(api): add new exports in index

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,3 +3,7 @@ export { UserModule } from './user/user.module';
 export { KnowledgeModule } from './knowledge/knowledge.module';
 
 export { AcademyModule } from './academy/academy.module';
+
+export { TicketModule } from './ticket/ticket.module';
+
+export { ReviewModule } from './review/review.module';
