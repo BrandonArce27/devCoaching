1c8e24dca0c543247793c26acdf5916388745d4e
BrandonArce27 <brandonarce2712@gmail.com>
2024-05-09 15:39:24 -0600

    feat(auth-guards): add context for user and authorization

--- src/shared/auth/guards/jwt-auth.guard.ts
+++ src/shared/auth/guards/jwt-auth.guard.ts
@@ -1,11 +1,39 @@
-import { ExecutionContext } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { GqlExecutionContext, Resolver } from '@nestjs/graphql';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtService } from '@nestjs/jwt';
 
 @Resolver()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  getRequest(context: ExecutionContext) {
-    const ctx = GqlExecutionContext.create(context);
-    return ctx.getContext().req;
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext) {
+    const graphqlContext = GqlExecutionContext.create(context);
+    const ctx = graphqlContext.getContext();
+
+    const token = this.extractTokenFromHeader(ctx.req);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+
+      ctx['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization']?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
